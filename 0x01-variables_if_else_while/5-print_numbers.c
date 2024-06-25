fbdc6ac9a7eb17c7fcb024b6e1436269924c0bfb
Devin Naicker <devinnaicker@gmail.com>
2024-06-25 05:26:22 -0700

    5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+/**
+ * main - Print numbers 0-9 on a new line
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n = 0;
+
+	while (n < 10)
+	{
+		printf("%d\n", n);
+		n++;
+	}
+
+	return (0);
+}
