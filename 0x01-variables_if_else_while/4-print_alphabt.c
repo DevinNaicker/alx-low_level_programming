9b93a2e93300261bffc4e1c63046ba70aa3463f4
Devin Naicker <devinnaicker@gmail.com>
2024-06-25 04:51:35 -0700

    4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+/**
+ * main - print aphabet in lower case except for q and e
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	char a = 'a'
+
+	while (a <= 'z', a != 'e', a != 'q')
+	{
+		putchar(a);
+		a++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
