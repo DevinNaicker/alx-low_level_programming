afd966f061d7ba37e87c85847704dd479e92b39c
Devin Naicker <devinnaicker@gmail.com>
2024-06-27 07:22:01 -0700

    0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,13 +8,15 @@
 
 int main(void)
 {
-	char *str = "_putchar\n";
-
-	while (*str)
-	{
-		_putchar(*str);
-		str++;
-	}
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
 	return (0);
 }
