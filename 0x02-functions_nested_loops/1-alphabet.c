09ece5b495d6a4a750cd408797c67fca6c3d6602
Devin Naicker <devinnaicker@gmail.com>
2024-06-27 06:23:46 -0700

    1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - function that prints the lower case alphabet and a new line
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet(void);
+{
+	char a = 'a';
+
+	while (a <= 'z')
+	{
+		_putchar(a);
+		a++;
+	}
+
+	_putchar('\n');
+
+	return (0);
+}
