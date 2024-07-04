d33f96cea53a7baaee083cf444f5e24271a24972
Devin Naicker <devinnaicker@gmail.com>
2024-07-04 13:20:59 -0700

    0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,13 +1,10 @@
 #include "main.h"
-
 /**
  * _strcat - concatenates two strings
  * @dest: destination
  * @src: source
- *
  * Return: the pointer to dest
  */
-
 char *_strcat(char *dest, char *src)
 {
 	int count =0, count1 =0;
